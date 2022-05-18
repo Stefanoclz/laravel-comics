a2d21349f5160eb5f0987071070af51b3864ee8b
Stefano Calzoni <stefanoclz@hotmail.it>
2022-05-18 15:25:32 +0200

    in creazione componente header

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -4,14 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
     <title>DC Comics</title>
 </head>
 <body>
 
-    <header>
-        @yield('nav')
-        @yield('jumbo')
-    <header>
+    @include('partials/header')
 
     <main>
         @yield('comicsList')
---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <div class="top-header">
+        <h4>
+            DC POWER VISA
+        </h5>
+        <select name="" id="">
+            <option value="<H5>ADDITIONAL DC SITES"><H5>ADDITIONAL DC SITES</option>
+        </H5></select>
+    </div>
+    <div class="container">
+      <img src="images/dc-logo.png" alt="DC logo" />
+      <ul>
+        <li>
+          <a href="#">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="#">COMICS</a>
+        </li>
+        <li>
+          <a href="#">MOVIES</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">GAMES</a>
+        </li>
+        <li>
+          <a href="#">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="#">VIDEOS</a>
+        </li>
+        <li>
+          <a href="#">FANS</a>
+        </li>
+        <li>
+          <a href="#">NEWS</a>
+        </li>
+        <li>
+          <a href="#">SHOP</a>
+        </li>
+      </ul>
+    </div>
+  </header>
