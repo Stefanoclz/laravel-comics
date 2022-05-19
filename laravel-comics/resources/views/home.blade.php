10bdf27b46a46f9783497ea2a35db2233cc8a70b
Stefano Calzoni <stefanoclz@hotmail.it>
2022-05-19 15:44:05 +0200

    esercizio completato

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>DC Comics</title>
-</head>
-<body>
+@extends('layout.layout')
 
     @include('partials.header')
 
@@ -24,6 +15,3 @@
     @include('partials.topFooter')
 
     @include('partials.footer')
-    
-</body>
-</html>
